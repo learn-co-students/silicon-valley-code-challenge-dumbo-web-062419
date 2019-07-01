a1148ea09377877e0d0912e0fcf962e2f9147a77
lgm527 <laurellmccaffrey@gmail.com>
2019-07-01 14:50:46 -0400

    Silicon Valley all mapped out

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,58 @@
 class VentureCapitalist
+  attr_accessor :name, :total_worth
+
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@all.select do |vc|
+      if vc.total_worth > 1000000000
+        vc
+      end
+    end
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select do |fr|
+      fr.venture_capitalist == self
+    end
+  end
+
+  def portfolio
+    port = []
+    self.funding_rounds.map do |fr|
+      port << fr.startup
+    end
+    port.uniq
+  end
+
+  def biggest_investment
+    sorted = self.funding_rounds.sort_by(&:investment)
+    sorted[-1]
+  end
+
+  def invested(domain)
+    total = 0
+    self.funding_rounds.each do |fr|
+      if fr.startup.domain == domain
+        total += fr.investment
+      end
+    end
+    total
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,32 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#FundingRound.new(startup, venture_capitalist, type, investment)
+#Startup.new(name, founder, domain)
+#VentureCapitalist.new(name, total_worth)
+
+startup1 = Startup.new("starter", "L", "domainA")
+startup2 = Startup.new("livelive", "Bob", "domainB")
+startup3 = Startup.new("rider", "Janet", "domainC")
+startup4 = Startup.new("gogogo", "Paul", "domainD")
+
+vc1 = VentureCapitalist.new("Randy", 55000000000.00)
+vc2 = VentureCapitalist.new("Kevin", 4399907.00)
+vc3 = VentureCapitalist.new("Michael", 565600.00)
+vc4 = VentureCapitalist.new("J", 1123000000.00)
+
+fround1 = FundingRound.new(startup1, vc1, "Angel", 1000000000.00)
+fround2 = FundingRound.new(startup2, vc2, "Pre-seed", 2000000000.00)
+fround3 = FundingRound.new(startup3, vc3, "Seed", 5000000.00)
+fround4 = FundingRound.new(startup4, vc4, "SeriesA", 33000000.00)
+
+startup1.sign_contract(vc3, "thetype", 4500.00)
+startup1.sign_contract(vc3, "hype", 100.00)
+startup1.sign_contract(vc4, "h", 9765870.00)
+
+vc1.offer_contract(startup2, "pre-seed", 123214.00)
+vc1.offer_contract(startup2, "seed", 21747484.00)
+vc1.offer_contract(startup2, "seed", 84.00)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
