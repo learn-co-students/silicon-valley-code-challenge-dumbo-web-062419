3ae82ca2bd97d2372346bf8f40aeb74ac701a26c
github username <github email address>
2019-07-01 14:33:56 -0400

    Almost done, up to venture_capitalist#biggest_investment

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,45 @@
 class VentureCapitalist
 
+    attr_accessor :total_worth
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_comma_club
+        @@all.select { |vc| vc.total_worth > 1000000000}
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select { |funding_round| funding_round.venture_capitalist == self }
+    end
+
+    def portfolio
+        all_my_startups = funding_rounds.map { |funding_round| funding_round.startup }
+        all_my_startups.uniq
+    end
+
+    def biggest_investment
+        biggest_funding_round = funding_rounds[0]
+        funding_rounds.each do |funding_round|
+            if funding_round.investment > biggest_funding_round.investment
+                biggest_funding_round = funding_round
+            end
+        end
+        biggest_funding_round
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,36 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup1 = Startup.new("Apple", "Steve Jobs", "apple.com")
+# startup1.pivot("google.com", "Google")
+startup2 = Startup.new("Google", "Sergey Brin", "google.com")
+Startup.find_by_founder("Steve Jobs")
+Startup.domains
+
+vc1 = VentureCapitalist.new("Warren Buffet", 2000000000)
+VentureCapitalist.tres_comma_club
+vc2 = VentureCapitalist.new("Bill Gates", 3000000000)
+vc3 = VentureCapitalist.new("Elon Musk", 4000000)
+
+funding_round1 = FundingRound.new(startup1, vc1, "Angel", 3000.0)
+
+########### Associations and Aggregate methods ###########
+
+funding_round2 = startup1.sign_contract(vc1, "Pre-Seed", 5000.0)
+funding_round3 = startup1.sign_contract(vc2, "Seed", 8000.0)
+funding_round4 = startup1.sign_contract(vc3, "Series A", 10000.0)
+
+startup1.my_funding_rounds
+startup1.num_funding_rounds
+startup1.total_funds
+startup1.investors
+startup1.big_investors
+
+funding_round5 = vc1.offer_contract(startup1, "Series B", 4000)
+
+vc1.funding_rounds
+vc1.portfolio
+vc1.biggest_investment
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
